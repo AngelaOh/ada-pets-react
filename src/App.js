1e9c00e29a95f68c9cb500f27f15a331b8a5e668
Angela <angela.ohh@gmail.com>
2019-06-17 14:49:09 -0700

    added details show using ternary

--- src/App.js
+++ src/App.js
@@ -20,12 +20,23 @@ class App extends Component {
     };
   }
 
+  showPetDetails = (id, name, species, about, location) => {
+     console.log(id, name, species, about, location)
+     let selectedPet = this.state.petList[id-1]
+     console.log('old selected pet', selectedPet)
 
+    this.setState({
+      currentPet: selectedPet
+    })
+    console.log('current pet', this.state.currentPet)
 
+  }
 
   render() {
     const { currentPet } = this.state;
     
+
+
     return (
       <main className="App">
         <header className="app-header">
@@ -36,8 +47,14 @@ class App extends Component {
           <SearchBar />
         </section>
           { /* Wave 2:  Where Pet Details should appear */ }
+        <section>
+          { currentPet !== undefined ? < PetDetails currentPet={currentPet} /> : '' }
+        </section>
         <section className="pet-list-wrapper">
           { /* Wave 1:  Where PetList should appear */ }
+          <PetList 
+          allPets={this.state.petList}
+          onSelectPet={this.showPetDetails} />
         </section>
         <section className="new-pet-form-wrapper">
           { /* Wave 3:  Where NewPetForm should appear */ }
--- src/components/PetList.js
+++ src/components/PetList.js
@@ -4,18 +4,37 @@ import PetCard from './PetCard';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 const PetList = (props) => {
 
+  const showPetDetails = (id, name, species, about, location) => {
+    console.log('in show pet details function')
+    props.onSelectPet(id, name, species, about, location)
+  }
 
+  const allPets = props.allPets.map((pet, i) => {
+
+    return (
+        <PetCard
+        // id={pet.id}
+        // name={pet.name}
+        // species={pet.species}
+        // about={pet.about}
+        // location={pet.location} 
+        {...pet}
+        petDetailsClickCallback={showPetDetails} />
+    )
+  })
+
+  
   return (
     <div className="card-group">
+      { allPets }
     </div>
   )
 }
 
 PetList.propTypes = {
-  pets: PropTypes.array.isRequired,
+  allPets: PropTypes.array.isRequired,
   onSelectPet: PropTypes.func,
 };
 
